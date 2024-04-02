058458114969d0a3f78422c3ad12382590d8faff
Veronika Matoušková <andrlovaver@gmail.com>
2024-04-02 18:32:59 +0200

    add js code

---
+++ index.js
@@ -0,0 +1,28 @@
+const formular = document.querySelector("#registrace")
+
+formular.addEventListener("input", (event) => {
+    const oblast = document.querySelector(".container")
+    const email = document.querySelector("#email")
+
+    if (event.target.value.length > 1 || email.value.includes("@")) {
+        oblast.classList.remove("red-border")
+    } else {
+        oblast.classList.add("red-border")
+    }
+})
+
+
+formular.addEventListener("submit", (event) => {
+    const emailValue = document.querySelector("#email").value
+    
+    if (emailValue.indexOf("@") === -1) {
+        event.preventDefault()
+        const oblast = document.querySelector(".container")
+        oblast.classList.add("red-border")
+    } else {
+        const oblast = document.querySelector(".container")
+        oblast.innerHTML = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${emailValue}.`
+    }
+})
+
+        oblast.classList.add("red-border")
